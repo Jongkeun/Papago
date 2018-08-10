8398def3b430a8e7caac07f128875a47d14df42f
Jongkeun <dldkfod@naver.com>
2018-08-10 17:47:46 +0900

    add httpRequest test function

--- Papago/MainWindow.xaml.cs
+++ Papago/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Net;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace Papago
 {
@@ -23,6 +28,29 @@ namespace Papago
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://openapi.naver.com");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("X-Naver-Client-Id", "id");
+            client.DefaultRequestHeaders.Add("X-Naver-Client-Secret", "screte");
+            Test(client);
+
+        }
+        private async void Test(HttpClient client)
+        {
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("query", txtInput.Text)
+            });
+            var result = await client.PostAsync("/v1/papago/detectLangs", content);
+            string resultContent = await result.Content.ReadAsStringAsync();
+            //return resultContent;
+            Console.Write(resultContent);
+            txtOutput.Text = resultContent;
         }
     }
 }
