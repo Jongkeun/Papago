1dfb544172cb1f22bed987e9b7bd9061141db225
jongkeun <dldkfod@naver.com>
2018-08-18 23:23:32 +0900

    set detect response data type

--- Papago/Module/PapagoApi.cs
+++ Papago/Module/PapagoApi.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace Papago
 {
@@ -27,8 +27,10 @@ namespace Papago
             {
                 new KeyValuePair<string, string>("query", inputData)
             });
-            var result = await client.PostAsync(PDefine.DETECT_API, content);            
-            return await result.Content.ReadAsStringAsync();
+            var result = await client.PostAsync(PDefine.DETECT_API, content);
+            string strJson = result.Content.ReadAsStringAsync().Result;
+            DetectRsp response = JsonConvert.DeserializeObject<DetectRsp>(strJson);
+            return response.langCode;
         }
 
         public async Task<string> fnTranslateLanguage(string inputData)
